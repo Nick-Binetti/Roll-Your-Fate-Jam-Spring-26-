2c7a619e9682f6611e21ec7a3e4d802154708857
Nicholas Binetti <nib1228@rit.edu>
2026-03-22 13:51:05 -0400

    1% chance to get accepted to job

--- is-this-hell/submit_button.gd
+++ is-this-hell/submit_button.gd
@@ -1,13 +1,19 @@
 extends Button
 
+@onready var acceptance = $"../../Job-offer-email"
 @onready var rejection = $"../../Job-rejection-email-visual"
 @onready var jobApp = $"../../Employment-job-application"
 
 func _on_pressed() -> void:
+	var randomChance = randi_range(1,100)
 	Global.appCount += 1
 	print(Global.appCount)
 	#delete job app
 	jobApp.queue_free()
 	#random logic 1%
-	rejection.show()
+	if randomChance == 1:
+		acceptance.show()
+	else:
+		print(randomChance)
+		rejection.show()
 	
